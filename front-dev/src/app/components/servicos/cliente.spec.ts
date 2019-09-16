826c7c5eca2d32967c7a12c3bebf0b4fd197c7e1
Marcelo <marcelo.souza@dce.ufpb.br>
2019-09-16 09:15:47 -0300

    Atualizando os testes em cliente.spec.ts

--- front-dev/src/app/components/servicos/cliente.spec.ts
+++ front-dev/src/app/components/servicos/cliente.spec.ts
@@ -1,14 +1,17 @@
 import { Cliente } from "../cliente/cliente";
 import { Endereco } from '../cliente/endereco';
 import { Telefone } from '../cliente/telefone';
+import { Locador } from '../locador/locador';
+import { Locatario } from '../locatario/locatario';
 
 describe('Cliente', () => {
     it('should create an instance', () => {
-        expect(new Cliente('Eduardo Pessoa', '', '', '', true, new Endereco('', '', '', ''), new Telefone('', ''), 'cpf')).toBeTruthy();
+        expect(new Locador('Eduardo Pessoa', '', '', '', 'true', new Endereco('', '', '', ''), new Telefone('', ''), 'cpf')).toBeTruthy();
 
     });
     it('should accept values in the constructor', () => {
-        let cliente = new Cliente('Eduardo Carreira', '', '', '', false, new Endereco('', '', '', ''), new Telefone('', ''), 'cnpj');
+        let cliente = new Locatario('Eduardo Carreira', '', '', '', 'false', new Endereco('', '', '', ''), new Telefone('', ''), 'cnpj');
+        
         expect(cliente.nome).toEqual('Eduardo Carreira');
         expect(cliente.cadastroPessoa).toEqual('cnpj');
         expect(cliente.id).toBeUndefined();
